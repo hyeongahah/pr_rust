ec78eab7eebe78b3e153733130967743dc647c60
songhyeongju <hyeongah9@gmail.com>
2024-11-26 09:20:03 +0900

    문법 설명 주석

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,12 +1,12 @@
-use std::io;
+use std::io; // 표준 라이브러리 크레이트의 IO 모듈을 사용
 
 fn main() {
     println!("Enter your weight (kg): ");
     let mut input = String::new();
 
     io::stdin().read_line(&mut input).unwrap();
-
     let weight: f32 = input.trim().parse().unwrap();
+    // mars_weight는 자체적으로 할당 받은 caculate 함수를 보고 유형을 정한다. 물론 직접 지정해줘도 된다.
     let mars_weight = calculate_weight_on_mars(weight);
     println!("Weight on Mars: {}kg", mars_weight);
 }
