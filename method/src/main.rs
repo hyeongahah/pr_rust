605a34619629c3e952936c67524a94675f343fbe
songhyeongju <hyeongah9@gmail.com>
2024-11-28 14:33:54 +0900

    method

---
+++ method/src/main.rs
@@ -0,0 +1,36 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    fn area(&self) -> u32 {
+        //여기에서 self는 Rectangle 구조체를 뜻함
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        // 두 도형의 크기를 비교해서 앞의 도형이 더 크면 true, 아니면 false
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    println!("The rectangle has s nonzero width; it is {}", rect1.width());
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    println!("Can rec1 hold rect2? {}", rect1.can_hold(&rect2));
+}
